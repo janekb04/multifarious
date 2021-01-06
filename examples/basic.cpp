7648e6d9b435c34eb01e6e3a574df13246777e5c
janekb04 <janekb04@icloud.com>
2021-01-06 19:31:21 +0100

    Add example

---
+++ examples/basic.cpp
@@ -0,0 +1,60 @@
+#include "multifarious.h"
+#include <iostream>
+#include <cassert>
+#include <numeric>
+
+int main() {
+    namespace mf = multifarious;
+
+    { // member_pointer_t
+        using namespace mf::type_traits;
+
+        // Pointer to method (non-static member function) of `std::string` having the signature `void()`
+        static_assert(std::is_same_v<
+                member_pointer_t<std::string, void()>,
+                void (std::string::*)()
+        >);
+    }
+
+    { // array_t
+        using namespace mf::type_traits;
+
+        // Array of 5 pointers to functions taking a char* and returning a pointer to an unbounded array of char*
+        char* (* (* foo_1[5])(char*))[];
+        array_t<std::add_pointer_t<std::add_pointer_t<array_t<char*>>(char*)>, 5> foo_2;
+        static_assert(std::is_same_v<decltype(foo_1), decltype(foo_2)>);
+    }
+
+    { // bit_cast
+        using namespace mf::util;
+
+        float f = 3.0f;
+        assert(bit_cast<int>(3.0f) == *(int*) (&f));
+    }
+
+    { // compose
+        using namespace mf::functional;
+
+        constexpr auto composed = compose([](int x) { return x * x; }, [](int x) { return x + 3; });
+        constexpr auto manual = [](int x) { return x * x + 6 * x + 9; };
+
+        for (int i = 0; i < 100; ++i) {
+            assert(composed(i) == manual(i)); // TODO: change to static_assert
+        }
+    }
+
+    { // y_combinator
+        using namespace mf::functional;
+
+        [[maybe_unused]] // for some unknown reason clang-tidy complains that `fib` is unused
+        constexpr auto fib = y_combinator([](auto&& fib, int x) {
+            if (x < 2) {
+                return 1;
+            } else {
+                return fib(fib, x - 1) + fib(fib, x - 2);
+            }
+        });
+
+        static_assert(fib(15) == 987);
+    }
+}
